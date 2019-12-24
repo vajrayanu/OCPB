22b688e05a0c2c11311dcee8a62d8e59b2d1ba13
Vajrayanu <yarnu.watchara@gmail.com>
2019-12-24 12:11:01 +0700

    Add files via upload

---
+++ OCPB/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+namespace OCPB
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // var enableCorsAttr = new EnableCorsAttribute(origins: "*", headers: "*", methods: "*") { SupportsCredentials = true };
+            // config.EnableCors(enableCorsAttr); 
+            // config.MapHttpAttributeRoutes();
+            // //config.Routes.MapHttpRoute(
+            // //    name: "DefaultApi",
+            // //    routeTemplate: "api/{controller}/{action}/{id}",
+            // //    defaults: new { controller = "Papi", action = "Get", id = RouteParameter.Optional }
+            // //);
+            // config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{action}/{id}",
+            //    defaults: new { id = RouteParameter.Optional, action = "Get" }
+            //);
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { action = "Get", id = RouteParameter.Optional }
+            );
+        }
+    }
+
+}
